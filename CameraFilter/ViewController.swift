d34f0700893040c4500d56f9bc62bf922bc834e4
Aleksander Waage <aleksander.waage@tv2.no>
2020-06-30 13:56:54 +0200

    Initial Commit

---
+++ CameraFilter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CameraFilter
+//
+//  Created by Aleksander Waage on 30/06/2020.
+//  Copyright © 2020 Waageweb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
