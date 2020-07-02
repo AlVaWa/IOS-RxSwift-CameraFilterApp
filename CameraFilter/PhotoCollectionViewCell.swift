4b3f558604f98039431b79cabf4420fd2648b334
Aleksander Waage <aleksander.waage@tv2.no>
2020-07-02 13:25:05 +0200

    Inital commit

---
+++ CameraFilter/PhotoCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCollectionViewCell.swift
+//  CameraFilter
+//
+//  Created by Aleksander Waage on 01/07/2020.
+//  Copyright © 2020 Waageweb. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var photoImageView: UIImageView!
+}
--- CameraFilter/ViewController.swift
+++ CameraFilter/ViewController.swift
@@ -7,12 +7,57 @@
 //
 
 import UIKit
+import RxSwift
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var filterButton: UIButton!
+    
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        photoImageView.isHidden = true
+        filterButton.isHidden = true
+        
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        guard let navC = segue.destination as? UINavigationController,
+            let photosCVC = navC.viewControllers.first as? PhotoSelectionViewController else {
+                fatalError("Segue destination is not found")
+        }
+        
+        photosCVC.selectedPhoto.subscribe(onNext: { [weak self] photo in
+            DispatchQueue.main.async {
+                self?.updateUI(with: photo)
+            }
+        }).disposed(by: disposeBag)
+    }
+    
+    @IBAction func applyFilterTapped(_ sender: Any) {
+        
+        guard let sourceImage = self.photoImageView.image else { return }
+        
+        FilterService().applyFilter(to: sourceImage)
+            .subscribe(onNext: { filterImage in
+            
+            DispatchQueue.main.async {
+                self.photoImageView.image = filterImage
+            }
+            
+            }).disposed(by: disposeBag)
+        
+    }
+    
+    private func updateUI(with image: UIImage) {
+        self.photoImageView.image = image
+        self.photoImageView.isHidden = false
+        self.filterButton.isHidden = false
     }
 
 
